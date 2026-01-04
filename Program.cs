4893f133efb0a29d719ce356f69cb506364ec2e3
karenluizam <luizamarques20@gmail.com>
2026-01-04 20:56:18 -0300

    Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone ip1 = new Iphone("92584-8541", "Iphone 20", "55515635544sdsdds", 254);
+Nokia nk1 = new Nokia("96325-4125", "Nokia 2003", "654987fvdvfd", 118);
+
+//Iphone
+
+Console.WriteLine($"IPHONE: {ip1.Numero}");
+ip1.InstalarAplicativo("Duolingo");
+ip1.Ligar();
+
+
+Console.WriteLine();
+//Nokia
+
+Console.WriteLine($"NOKIA: {nk1.Numero}");
+nk1.InstalarAplicativo("Facebook Lite");
+nk1.ReceberLigacao();
